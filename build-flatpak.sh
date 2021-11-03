2add70375b04615f4057108cb16d9cda728c3f65
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-11-03 21:37:18 +0100

    add build-flatpak.sh

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-flatpak-builder flatpak-build-dir io.github.com.igorlogius.scr2ppm.yml
-#flatpak-builder --user --install --force-clean flatpak-build-dir io.github.com.igorlogius.scr2ppm.yml
-#flatpak run io.github.com.igorlogius.scr2ppm.yml
+flatpak-builder flatpak-build-dir --force-clean io.github.com.igorlogius.scr2ppm.yml
+flatpak-builder --user --install --force-clean flatpak-build-dir io.github.com.igorlogius.scr2ppm.yml
+flatpak run io.github.com.igorlogius.scr2ppm
 
