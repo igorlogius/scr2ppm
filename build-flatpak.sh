157b26757cf4ebf48fdce1f84d5581c510a296cd
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-11-04 15:22:01 +0100

    still preping stuff for flatpak release

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-flatpak-builder flatpak-build-dir --force-clean io.github.com.igorlogius.scr2ppm.yml
-flatpak-builder --user --install --force-clean flatpak-build-dir io.github.com.igorlogius.scr2ppm.yml
-flatpak run io.github.com.igorlogius.scr2ppm
-
+flatpak-builder flatpak-build-dir --force-clean *.yml
+flatpak-builder --user --install --force-clean flatpak-build-dir *.yml
+flatpak run $(basename -s ".yml" *.yml)
